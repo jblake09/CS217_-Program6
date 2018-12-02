2097b08051172e1dc3e2401b6cf7c802d7def324
Jeffrey E Blake Jr <jeffrey.e.blake@gmail.com>
2018-12-02 17:10:51 -0600

    workingon readVal

--- PG6.asm
+++ PG6.asm
@@ -12,7 +12,7 @@ INCLUDE Irvine32.inc
 
 ; (insert constant definitions here)
 
-SIZE = 10
+ARRAY_SIZE = 10
 
 displayString    MACRO buffer
 	push	edx                    ;from lecture example
@@ -25,7 +25,7 @@ mReadStr	MACRO varName			; from lecture example
 	push	ecx
 	push	edx
 	mov		edx, OFFSET varName
-	mov		ecx, (SIZEOF varName) – 1 
+	mov		ecx, (SIZEOF varName) - 1
 	call	ReadString
 	pop		edx
 	pop		ecx
@@ -42,16 +42,18 @@ instruct4	BYTE	"of the integers, their sum, and their average value.", 0
 prompt		BYTE	"Please enter an unsigned number: ", 0
 error		BYTE	"ERROR: You did not enter an unsigned number or your number was too big."
 tryAgain	BYTE	"Please try again: ", 0
-myNums		DWORD	MAX		DUP(?)
+myNums		DWORD	ARRAY_SIZE		DUP(?)
 inString	BYTE	30		DUP(?)
 tempNum		DWORD	?
 sum			DWORD	0
 space		BYTE	", ", 0
+bye			BYTE	"BYE! Thanks for playing!", 0
 
 
 .code
 main PROC
 
+;Display the title/author and program instructions
 	push	OFFSET instruct4
 	push	OFFSET instruct3
 	push	OFFSET instruct2
@@ -59,9 +61,13 @@ main PROC
 	push	OFFSET author
 	push	OFFSET progTitle
 	call	intro
+;Populate the aray with users numbers
 	push	OFFSET prompt
 	push	OFFSET myNums
 	call	readVal
+;Dsiplay a farewell message
+	push	OFFSET bye
+	call	farewell
 
 	exit	; exit to operating system
 main ENDP
@@ -69,7 +75,7 @@ main ENDP
 ; (insert additional procedures here)
 ;*********************************************************************************************
 ;Procedure to introduce the program ,and give user instructions.
-;receives: the program title and intructions
+;receives: the program title and intructions, and author name
 ;returns: prints the program title, author name, and  instructions
 ;preconditions:  none
 ;registers changed: edx
@@ -101,8 +107,8 @@ intro	ENDP
 
 ;*********************************************************************************************
 ;Procedure to get 10 numbers inputted from user
-;receives: the program title and intructions
-;returns: prints the program title, author name, and  instructions
+;receives: array of numbers
+;returns: user inputted numbers inserted into array
 ;preconditions:  none
 ;registers changed: edx
 ;*********************************************************************************************
@@ -111,10 +117,35 @@ readVal	PROC
 ;Set up stack
 	push	ebp
 	mov		ebp, esp
+	mov		edi, [ebp+12]	;numbr array in edi
+	mov		ecx, ARRAY_SIZE
+askInput:
+	displayString	[ebp+8]
 
 	pop		ebp
 	ret		8
 
-ReadVal	ENDP
+readVal	ENDP
+
+;*********************************************************************************************
+;Procedure to display a farewell message
+;receives: the the farewell message
+;returns: prints the farewell message
+;preconditions:  none
+;registers changed: edx
+;*********************************************************************************************
+farewell	PROC
+
+;Set up stack
+	push	ebp
+	mov		ebp, esp
+	call	CrLf
+	call	CrLf
+	displayString	[ebp+8]
+	call	CrLf
+	pop		ebp
+	ret		4
+
+farewell	ENDP
 
 END main
